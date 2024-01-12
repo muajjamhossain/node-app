f311a2f231408868d4292c696a0e878b47b9456a
muajjamhossain <muajjam.imu@gmail.com>
2024-01-13 00:38:38 +0600

    code formate

---
+++ helpers/handleReqRes.js
@@ -0,0 +1,27 @@
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
+
+const handler = {};
+handler.handleReqRes = (req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headersObject = req.headers;
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on('end', () => {
+        realData += decoder.end();
+        console.log(realData);
+        res.end('hello world');
+    });
+
+    // res.end('hello world');
+};
+
+module.exports = handler;
